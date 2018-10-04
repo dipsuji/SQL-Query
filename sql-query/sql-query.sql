8b9e5ccccc2c5e394400d1a5386cf2e181dab86e
Sujata Maurya <mauryasujata69@gmail.com>
2018-10-04 22:34:19 +0900

    db query
    insert in to, update, not null example.

--- sql-query/sql-query.sql
+++ sql-query/sql-query.sql
@@ -8,6 +8,25 @@ SELECT  roll_no, name FROM `student-database`.student_table WHERE  NOT age<25;
 SELECT  age, name FROM `student-database`.student_table WHERE  NOT age<25 ORDER BY age;
 SELECT  roll_no, name FROM `student-database`.student_table  ORDER BY roll_no DESC;
 
+INSERT INTO `student-database`.student_table (name,  age, roll_no)
+VALUES ("Mike", 34, 98989) ;
+
+SELECT sr_no, name
+FROM `student-database`.student_table
+WHERE adress IS NULL;
+
+SELECT sr_no, name
+FROM `student-database`.student_table
+WHERE adress IS NOT NULL;
+
+UPDATE `student-database`.student_table
+SET name = "Jhon1"
+WHERE sr_no = 7;
+
+UPDATE `student-database`.student_table
+SET name = "Sora"
+WHERE sr_no = 8;
+
 
 
 
