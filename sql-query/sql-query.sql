dc3e83841a3cca8ebb165bba14d3759192e36ec7
Sujata Maurya <mauryasujata69@gmail.com>
2018-10-09 23:25:07 +0900

    more SQL queries
    example-
    SELECT tt.firstname, st.name
    FROM `student-database`.techer_table AS tt, `student-database`.student_table AS st
    WHERE tt.address = st.adress;

--- sql-query/sql-query.sql
+++ sql-query/sql-query.sql
@@ -50,6 +50,84 @@ FROM `student-database`.techer_table;
 SELECT SUM(salary) as sumoftecherssalary
 FROM `student-database`.techer_table;
 
+SELECT * FROM `student-database`.student_table
+WHERE name LIKE 'b%';
+
+SELECT * FROM `student-database`.student_table
+WHERE name LIKE '%a';
+
+SELECT * FROM `student-database`.student_table
+WHERE name LIKE '%or%';
+
+SELECT * FROM `student-database`.student_table
+WHERE name LIKE '_u%';
+
+SELECT * FROM `student-database`.student_table
+WHERE name LIKE 'a_%_%';
+
+SELECT * FROM `student-database`.student_table
+WHERE name LIKE 'a%x';
+
+SELECT * FROM `student-database`.student_table
+WHERE name NOT LIKE 'a%';
+
+SELECT * FROM `student-database`.student_table
+WHERE name LIKE 'din%';
+
+SELECT * FROM `student-database`.student_table
+WHERE name LIKE '%ba%';
+
+SELECT * FROM `student-database`.student_table
+WHERE name LIKE '_inkar';
+
+SELECT * FROM `student-database`.student_table
+WHERE name LIKE 's_j_ta';
+
+SELECT * FROM `student-database`.student_table
+WHERE name LIKE '[sda]%';
+
+SELECT * FROM `student-database`.student_table
+WHERE name LIKE '[a-b]%';
+
+SELECT * FROM `student-database`.student_table
+WHERE name LIKE '[!sda]%';
+
+SELECT * FROM `student-database`.student_table
+WHERE name NOT LIKE '[sda]%';
+
+SELECT * FROM `student-database`.student_table
+WHERE adress IN ('tokyo', 'sar', 'edogawa');
+
+SELECT * FROM `student-database`.student_table
+WHERE adress NOT IN ('tokyo', 'sar', 'edogawa');
+
+SELECT * FROM `student-database`.student_table
+WHERE age BETWEEN 22 AND 27;
+
+SELECT * FROM `student-database`.student_table
+WHERE age NOT BETWEEN 22 AND 27;
+
+SELECT * FROM `student-database`.student_table
+WHERE (age BETWEEN 22 AND 27)
+AND NOT sr_no IN (1,2,3);
+
+SELECT * FROM `student-database`.student_table
+WHERE sr_no BETWEEN 3 AND 4;
+
+SELECT * FROM `student-database`.student_table
+WHERE admissondate BETWEEN '2018-06-01' AND '2018-08-02';
+
+SELECT sr_no AS serial_number, name AS student_name
+FROM `student-database`.student_table;
+
+SELECT name AS student_name, roll_no AS roll_number FROM `student-database`.student_table;
+
+SELECT name, adress + ', '+ adress+ ',' + age + ', ' + admissondate AS Details
+FROM `student-database`.student_table;
+
+SELECT tt.firstname, st.name
+FROM `student-database`.techer_table AS tt, `student-database`.student_table AS st
+WHERE tt.address = st.adress;
 
 
 
